84e8d03ae6c8c8934e347ea756f78d2edcafd819
Alex <34255775+Poipole@users.noreply.github.com>
2017-12-05 19:17:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,14 @@ client.on('message', message => {
     	message.channel.send('lmao');
   	}
 });
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
 //Test Features! ^^^
 
 // THIS  MUST  BE  THIS  WAY
