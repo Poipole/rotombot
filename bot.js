eab8d05535d081ac389f6bf439a01443fbfffc0e
Alex <34255775+Poipole@users.noreply.github.com>
2017-12-20 20:55:25 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-    //no regrets
-    client.user.setGame(`Test!`);
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
 client.on('message', message => {
