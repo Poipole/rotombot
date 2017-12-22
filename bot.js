8c00a3403efba38e10f972f6bc04357f66e65491
Alex <34255775+Poipole@users.noreply.github.com>
2017-12-21 21:05:31 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 const token = 'Mzg3MzU4NjAyMzQxNTE1MjY0.DR39yQ.XEOna6oFsMLbGkiMqrDfaq5CeDE';
+require('events').EventEmitter.defaultMaxListeners = Infinity;
 
 client.on("ready", () => {
 console.log('I am ready!');
