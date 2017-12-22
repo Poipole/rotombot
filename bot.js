694e4c297316a796186eb69d8b2f6add82356b0d
Alex <34255775+Poipole@users.noreply.github.com>
2017-12-22 13:44:45 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,11 @@ client.on("ready", () => {
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-  // Send the message to the guilds default channel (usually #general), mentioning the member
-  member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
+const channel = member.guild.channels.find('name', 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
 });
                                      
 client.on('message', message => {
