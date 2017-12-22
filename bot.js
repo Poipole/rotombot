fa10ff81374af9494b4b3896b0fb79341b0581e6
Alex <34255775+Poipole@users.noreply.github.com>
2017-12-22 13:56:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ client.on('message', message => {
 //Test Features! ^^^
 client.on('message', message => {
     if (message.content === '!commands') {
-    	message.author.send('Hello\n``` -!rotom\n- !pineapple\n- !fusions\n- !soap\n- !soapy\n- !thegays\n- !fufu\n- !disgusting\n- !gaypowerlevel\n- !io\n- !getavatar```');
+    	message.author.send("Hello! \n``` - !rotom\n- !pineapple\n- !fusions\n- !soap\n- !soapy\n- !thegays\n- !fufu\n- !disgusting\n- !gaypowerlevel\n- !io\n- !getavatar```");
   	}
 });
 
