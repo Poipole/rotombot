cbff2eb0d0c474e219030d69d31204457b14b657
Alex <34255775+Poipole@users.noreply.github.com>
2017-12-21 21:07:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,19 @@ client.on("ready", () => {
 console.log('I am ready!');
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to the guilds default channel (usually #general), mentioning the member
+  member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
+
+  // If you want to send the message to a designated channel on a server instead
+  // you can do the following:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 client.on('message', message => {
   if (message.content === '!getavatar') {
     // Send the user's avatar URL
