f7f8d180485c501d0df8df1e7971827096af82b3
Alex <34255775+Poipole@users.noreply.github.com>
2017-12-21 20:40:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,18 +7,6 @@ client.on("ready", () => {
 console.log('I am ready!');
 });
 
-client.on('guildMemberAdd', member => {
-  // Send the message to the guilds default channel (usually #general), mentioning the member
-  member.guild.defaultChannel.mention(`Welcome to the server, ${member}!`);
-
-  // If you want to send the message to a designated channel on a server instead
-  // you can do the following:
-  const channel = member.guild.channels.find('name', 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
-});
 //offical features ^
 client.on('message', message => {
     if (message.content === '!rotom') {
