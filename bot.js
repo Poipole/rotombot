41698969d68c3c1ef773bb20743168632f8c071d
Alex <34255775+Poipole@users.noreply.github.com>
2018-01-02 20:38:44 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,28 +102,7 @@ client.on('message', message => {
   	}
 });
 
-//test
-client.on('message', message => {
-        if (message.content === '!kick') {
-            if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    let reason = args.slice(1).join(' ');
-    if(!reason)
-      return message.reply("Please indicate a reason for the kick!");
-    
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-  }
-
+//TESTING
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
